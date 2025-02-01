b97838bcc74a144dc17c02554eaf66454fa69243
hugo <devhugok@gmail.com>
2025-02-01 12:40:41 +0100

    Ajouter cards à page d'acceuil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class HomeController extends AbstractController {
+class HomeController extends AbstractController
+{
 
-    #[Route("/", name:"home")]
-    function index (Request $request) : Response {
-        return $this->render('home/index.html.twig');
-    }
+  #[Route("/", name: "home")]
+  function index(Request $request, RecipeRepository $recipeRepository): Response
+  {
+    $recipes = $recipeRepository->findAll();
+    return $this->render('home/index.html.twig', [
+      'recipes' => $recipes
+    ]);
+  }
 }
